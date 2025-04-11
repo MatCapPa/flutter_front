fa6fc636d0ca1486cde485607aab2268ae1f11d6
Matias Cappa <cappa.nicolas1@gmail.com>
2025-04-11 15:50:17 -0300

    frist commit

---
+++ lib/screens/screens.dart
@@ -0,0 +1,3 @@
+export 'package:flutter_front/screens/albums_screen.dart';
+export 'package:flutter_front/screens/home_screen.dart';
+export 'package:flutter_front/screens/album_single_screen.dart';
